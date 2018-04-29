7be9214b22be396aa138c5760374f8a9766cf0e4
miquelalcon <miquel.1996@gmail.com>
2018-04-29 23:13:56 +0200

    mlp.h adapted

--- mlp.h
+++ mlp.h
@@ -28,11 +28,11 @@ using namespace dynet;
  * Common activation functions used in multilayer perceptrons
  */
 enum Activation {
-  SIGMOID, /**< `SIGMOID` : Sigmoid function \f$x\longrightarrow \frac {1} {1+e^{-x}}\f$ */
-  TANH, /**< `TANH` : Tanh function \f$x\longrightarrow \frac {1-e^{-2x}} {1+e^{-2x}}\f$ */
-  RELU, /**< `RELU` : Rectified linear unit \f$x\longrightarrow \max(0,x)\f$ */
-  LINEAR, /**< `LINEAR` : Identity function \f$x\longrightarrow x\f$ */
-  SOFTMAX /**< `SOFTMAX` : Softmax function \f$\textbf{x}=(x_i)_{i=1,\dots,n}\longrightarrow \frac {e^{x_i}}{\sum_{j=1}^n e^{x_j} })_{i=1,\dots,n}\f$ */
+	SIGMOID, /**< `SIGMOID` : Sigmoid function \f$x\longrightarrow \frac {1} {1+e^{-x}}\f$ */
+	TANH, /**< `TANH` : Tanh function \f$x\longrightarrow \frac {1-e^{-2x}} {1+e^{-2x}}\f$ */
+	RELU, /**< `RELU` : Rectified linear unit \f$x\longrightarrow \max(0,x)\f$ */
+	LINEAR, /**< `LINEAR` : Identity function \f$x\longrightarrow x\f$ */
+	SOFTMAX /**< `SOFTMAX` : Softmax function \f$\textbf{x}=(x_i)_{i=1,\dots,n}\longrightarrow \frac {e^{x_i}}{\sum_{j=1}^n e^{x_j} })_{i=1,\dots,n}\f$ */
 };
 
 /**
@@ -44,24 +44,24 @@ enum Activation {
  */
 struct Layer {
 public:
-  unsigned input_dim; /**< Input dimension */
-  unsigned output_dim; /**< Output dimension */
-  Activation activation = LINEAR; /**< Activation function */
-  float dropout_rate = 0; /**< Dropout rate */
-  /**
-   * \brief Build a feed forward layer
-   *
-   * \param input_dim Input dimension
-   * \param output_dim Output dimension
-   * \param activation Activation function
-   * \param dropout_rate Dropout rate
-   */
-  Layer(unsigned input_dim, unsigned output_dim, Activation activation, float dropout_rate) :
-    input_dim(input_dim),
-    output_dim(output_dim),
-    activation(activation),
-    dropout_rate(dropout_rate) {};
-  Layer() {};
+	unsigned input_dim; /**< Input dimension */
+	unsigned output_dim; /**< Output dimension */
+	Activation activation = LINEAR; /**< Activation function */
+	float dropout_rate = 0; /**< Dropout rate */
+	/**
+	 * \brief Build a feed forward layer
+	 *
+	 * \param input_dim Input dimension
+	 * \param output_dim Output dimension
+	 * \param activation Activation function
+	 * \param dropout_rate Dropout rate
+	 */
+	Layer(unsigned input_dim, unsigned output_dim, Activation activation, float dropout_rate) :
+		input_dim(input_dim),
+		output_dim(output_dim),
+		activation(activation),
+		dropout_rate(dropout_rate) {};
+	Layer() {};
 };
 
 /**
@@ -72,202 +72,277 @@ public:
  */
 struct MLP {
 protected:
-  // Hyper-parameters
-  unsigned LAYERS = 0;
+	// Hyper-parameters
+	unsigned LAYERS = 0;
 
-  // Layers
-  vector<Layer> layers;
-  // Parameters
-  vector<vector<Parameter>> params;
+	// Layers
+	vector<Layer> layers;
+	vector<Layer> union_layers;
+	// Parameters
+	vector<vector<Parameter>> params;
+	vector<vector<Parameter>> union_params;
 
-  bool dropout_active = true;
+	bool dropout_active = true;
 
 public:
-  /**
-   * \brief Default constructor
-   * \details Dont forget to add layers!
-   */
-  MLP(ParameterCollection & model) {
-    LAYERS = 0;
-  }
-  /**
-   * \brief Returns a Multilayer perceptron
-   * \details Creates a feedforward multilayer perceptron based on a list of layer descriptions
-   *
-   * \param model ParameterCollection to contain parameters
-   * \param layers Layers description
-   */
-  MLP(ParameterCollection& model,
-      vector<Layer> layers) {
-    // Verify layers compatibility
-    for (unsigned l = 0; l < layers.size() - 1; ++l) {
-      if (layers[l].output_dim != layers[l + 1].input_dim)
-        throw invalid_argument("Layer dimensions don't match");
-    }
+	/**
+	 * \brief Default constructor
+	 * \details Dont forget to add layers!
+	 */
+	MLP(ParameterCollection & model) {
+		LAYERS = 0;
+	}
+	/**
+	 * \brief Returns a Multilayer perceptron
+	 * \details Creates a feedforward multilayer perceptron based on a list of layer descriptions
+	 *
+	 * \param model ParameterCollection to contain parameters
+	 * \param layers Layers description
+	 */
+	MLP(ParameterCollection& model,
+			vector<Layer> layers, vector<Layer> union_layers) {
+		// Verify layers compatibility
+		for (unsigned l = 0; l < layers.size() - 1; ++l) {
+			if (layers[l].output_dim != layers[l + 1].input_dim)
+				throw invalid_argument("Layer dimensions don't match");
+		}
 
-    // Register parameters in model
-    for (Layer layer : layers) {
-      append(model, layer);
-    }
-  }
+		// Register parameters in model
+		for (Layer layer : layers) {
+			siamese_append(model, layer);
+		}
 
-  /**
-   * \brief Append a layer at the end of the network
-   * \details [long description]
-   *
-   * \param model [description]
-   * \param layer [description]
-   */
-  void append(ParameterCollection& model, Layer layer) {
-    // Check compatibility
-    if (LAYERS > 0)
-      if (layers[LAYERS - 1].output_dim != layer.input_dim)
-        throw invalid_argument("Layer dimensions don't match");
+		// Verify union_layers compatibility
+		for (unsigned l = 0; l < union_layers.size() - 1; ++l) {
+			if (union_layers[l].output_dim != union_layers[l + 1].input_dim)
+				throw invalid_argument("Layer dimensions don't match");
+		}
 
-    // Add to layers
-    layers.push_back(layer);
-    LAYERS++;
-    // Register parameters
-    Parameter W = model.add_parameters({layer.output_dim, layer.input_dim});
-    Parameter b = model.add_parameters({layer.output_dim});
-    params.push_back({W, b});
-  }
+		// Register union_parameters in model
+		for (Layer layer : union_layers) {
+			union_append(model, layer);
+		}
+	}
 
-  /**
-   * \brief Run the MLP on an input vector/batch
-   *
-   * \param x Input expression (vector or batch)
-   * \param cg Computation graph
-   *
-   * \return [description]
-   */
-  Expression run(Expression x,
-                 ComputationGraph& cg) {
-    // Expression for the current hidden state
-    Expression h_cur = x;
-    for (unsigned l = 0; l < LAYERS; ++l) {
-      // Initialize parameters in computation graph
-      Expression W = parameter(cg, params[l][0]);
-      Expression b = parameter(cg, params[l][1]);
-      // Aplly affine transform
-      Expression a = affine_transform({b, W, h_cur});
-      // Apply activation function
-      Expression h = activate(a, layers[l].activation);
-      // Take care of dropout
-      Expression h_dropped;
-      if (layers[l].dropout_rate > 0) {
-        if (dropout_active) {
-          // During training, drop random units
-          Expression mask = random_bernoulli(cg, {layers[l].output_dim}, 1 - layers[l].dropout_rate);
-          h_dropped = cmult(h, mask);
-        } else {
-          // At test time, multiply by the retention rate to scale
-          h_dropped = h * (1 - layers[l].dropout_rate);
-        }
-      } else {
-        // If there's no dropout, don't do anything
-        h_dropped = h;
-      }
-      // Set current hidden state
-      h_cur = h_dropped;
-    }
+	/**
+	 * \brief Append a layer at the end of the network
+	 * \details [long description]
+	 *
+	 * \param model [description]
+	 * \param layer [description]
+	 */
+	void siamese_append(ParameterCollection& model, Layer layer) {
+		// Check compatibility
+		if (LAYERS > 0)
+			if (layers[LAYERS - 1].output_dim != layer.input_dim)
+				throw invalid_argument("Layer dimensions don't match");
 
-    return h_cur;
-  }
+		// Add to layers
+		layers.push_back(layer);
+		LAYERS++;
+		// Register parameters
+		Parameter W = model.add_parameters({layer.output_dim, layer.input_dim});
+		Parameter b = model.add_parameters({layer.output_dim});
+		params.push_back({W, b});
+	}
 
-  /**
-   * \brief Return the negative log likelihood for the (batched) pair (x,y)
-   * \details For a batched input \f$\{x_i\}_{i=1,\dots,N}\f$, \f$\{y_i\}_{i=1,\dots,N}\f$, this computes \f$\sum_{i=1}^N \log(P(y_i\vert x_i))\f$ where \f$P(\textbf{y}\vert x_i)\f$ is modelled with $\mathrm{softmax}(MLP(x_i))$
-   *
-   * \param x Input batch
-   * \param labels Output labels
-   * \param cg Computation graph
-   * \return Expression for the negative log likelihood on the batch
-   */
-  Expression get_nll(Expression x,
-                    vector<unsigned> labels,
-                    ComputationGraph& cg) {
-    // compute output
-    Expression y = run(x, cg);
-    // Do softmax
-    Expression losses = pickneglogsoftmax(y, labels);
-    // Sum across batches
-    return sum_batches(losses);
-  }
+	void union_append(ParameterCollection& model, Layer layer) {
+		// Check compatibility
+		if (LAYERS > 0)
+			if (union_layers[LAYERS - 1].output_dim != layer.input_dim)
+				throw invalid_argument("Layer dimensions don't match");
 
-  /**
-   * \brief Predict the most probable label
-   * \details Returns the argmax of the softmax of the networks output
-   *
-   * \param x Input
-   * \param cg Computation graph
-   *
-   * \return Label index
-   */
-  int predict(Expression x,
-              ComputationGraph& cg) {
-    // run MLP to get class distribution
-    Expression y = run(x, cg);
-    // Get values
-    vector<float> probs = as_vector(cg.forward(y));
-    // Get argmax
-    unsigned argmax = 0;
-    for (unsigned i = 1; i < probs.size(); ++i) {
-      if (probs[i] > probs[argmax])
-        argmax = i;
-    }
+		// Add to layers
+		union_layers.push_back(layer);
+		LAYERS++;
+		// Register parameters
+		Parameter W = model.add_parameters({layer.output_dim, layer.input_dim});
+		Parameter b = model.add_parameters({layer.output_dim});
+		union_params.push_back({W, b});
+	}
 
-    return argmax;
-  }
+	/**
+	 * \brief Run the MLP on an input vector/batch
+	 *
+	 * \param x Input expression (vector or batch)
+	 * \param cg Computation graph
+	 *
+	 * \return [description]
+	 */
+	Expression single_siamese_run(Expression x,
+									ComputationGraph& cg) {
+		// Expression for the current hidden state
+		Expression h_cur = x;
+		for (unsigned l = 0; l < LAYERS; ++l) {
+			// Initialize parameters in computation graph
+			Expression W = parameter(cg, params[l][0]);
+			Expression b = parameter(cg, params[l][1]);
+			// Aplly affine transform
+			Expression a = affine_transform({b, W, h_cur});
+			// Apply activation function
+			Expression h = activate(a, layers[l].activation);
+			// Take care of dropout
+			Expression h_dropped;
+			if (layers[l].dropout_rate > 0) {
+				if (dropout_active) {
+					// During training, drop random units
+					Expression mask = random_bernoulli(cg, {layers[l].output_dim}, 1 - layers[l].dropout_rate);
+					h_dropped = cmult(h, mask);
+				} else {
+					// At test time, multiply by the retention rate to scale
+					h_dropped = h * (1 - layers[l].dropout_rate);
+				}
+			} else {
+				// If there's no dropout, don't do anything
+				h_dropped = h;
+			}
+			// Set current hidden state
+			h_cur = h_dropped;
+		}
 
-  /**
-   * \brief Enable dropout
-   * \details This is supposed to be used during training or during testing if you want to sample outputs using montecarlo
-   */
-  void enable_dropout() {
-    dropout_active = true;
-  }
+		return h_cur;
+	}
 
-  /**
-   * \brief Disable dropout
-   * \details Do this during testing if you want a deterministic network
-   */
-  void disable_dropout() {
-    dropout_active = false;
-  }
+	Expression union_run(Expression x,
+									ComputationGraph& cg) {
+		// Expression for the current hidden state
+		Expression h_cur = x;
+		for (unsigned l = 0; l < LAYERS; ++l) {
+			// Initialize parameters in computation graph
+			Expression W = parameter(cg, union_params[l][0]);
+			Expression b = parameter(cg, union_params[l][1]);
+			// Aplly affine transform
+			Expression a = affine_transform({b, W, h_cur});
+			// Apply activation function
+			Expression h = activate(a, union_layers[l].activation);
+			// Take care of dropout
+			Expression h_dropped;
+			if (union_layers[l].dropout_rate > 0) {
+				if (dropout_active) {
+					// During training, drop random units
+					Expression mask = random_bernoulli(cg, {union_layers[l].output_dim}, 1 - union_layers[l].dropout_rate);
+					h_dropped = cmult(h, mask);
+				} else {
+					// At test time, multiply by the retention rate to scale
+					h_dropped = h * (1 - union_layers[l].dropout_rate);
+				}
+			} else {
+				// If there's no dropout, don't do anything
+				h_dropped = h;
+			}
+			// Set current hidden state
+			h_cur = h_dropped;
+		}
 
-  /**
-   * \brief Check wether dropout is enabled or not
-   *
-   * \return Dropout state
-   */
-  bool is_dropout_enabled() {
-    return dropout_active;
-  }
+		return h_cur;
+	}
+
+	Expression expressions_fusion(Expression a, Expression b, ComputationGraph& cg) {
+		float *p_a = a.value().v;
+		float *p_b = b.value().v;
+		vector<float> fusion = vector<float> (p_a, p_a+a.dim().size());
+		fusion.insert(fusion.end(), p_b, p_b + b.dim().size());
+
+		return input(cg, {800}, fusion);
+	}
+
+	/**
+	 * \brief Return the negative log likelihood for the (batched) pair (x,y)
+	 * \details For a batched input \f$\{x_i\}_{i=1,\dots,N}\f$, \f$\{y_i\}_{i=1,\dots,N}\f$, this computes \f$\sum_{i=1}^N \log(P(y_i\vert x_i))\f$ where \f$P(\textbf{y}\vert x_i)\f$ is modelled with $\mathrm{softmax}(MLP(x_i))$
+	 *
+	 * \param x Input batch
+	 * \param labels Output labels
+	 * \param cg Computation graph
+	 * \return Expression for the negative log likelihood on the batch
+	 */
+	Expression get_nll(Expression x1, Expression x2,
+										vector<unsigned> labels,
+										ComputationGraph& cg) {
+		// compute output
+		Expression y1 = single_siamese_run(x1, cg);
+		Expression y2 = single_siamese_run(x2, cg);
+		Expression y = expressions_fusion(y1,y2,cg);
+		// Do softmax
+		Expression losses = pickneglogsoftmax(y, labels);
+		// Sum across batches
+		return sum_batches(losses);
+	}
+
+	/**
+	 * \brief Predict the most probable label
+	 * \details Returns the argmax of the softmax of the networks output
+	 *
+	 * \param x Input
+	 * \param cg Computation graph
+	 *
+	 * \return Label index
+	 */
+	int predict(Expression x1, Expression x2,
+							ComputationGraph& cg) {
+		// run MLP to get class distribution
+		Expression y1 = single_siamese_run(x1, cg);
+		Expression y2 = single_siamese_run(x2, cg);
+		Expression y = expressions_fusion(y1,y2,cg);
+		// Get values
+		vector<float> probs = as_vector(cg.forward(y));
+		// Get argmax
+		// unsigned argmax = 0;
+		// for (unsigned i = 1; i < probs.size(); ++i) {
+		// 	if (probs[i] > probs[argmax])
+		// 		argmax = i;
+		// }
+
+		return probs[0] >= 0.5f;
+	}
+
+	/**
+	 * \brief Enable dropout
+	 * \details This is supposed to be used during training or during testing if you want to sample outputs using montecarlo
+	 */
+	void enable_dropout() {
+		dropout_active = true;
+	}
+
+	/**
+	 * \brief Disable dropout
+	 * \details Do this during testing if you want a deterministic network
+	 */
+	void disable_dropout() {
+		dropout_active = false;
+	}
+
+	/**
+	 * \brief Check wether dropout is enabled or not
+	 *
+	 * \return Dropout state
+	 */
+	bool is_dropout_enabled() {
+		return dropout_active;
+	}
 
 private:
-  inline Expression activate(Expression h, Activation f) {
-    switch (f) {
-    case LINEAR:
-      return h;
-      break;
-    case RELU:
-      return rectify(h);
-      break;
-    case SIGMOID:
-      return logistic(h);
-      break;
-    case TANH:
-      return tanh(h);
-      break;
-    case SOFTMAX:
-      return softmax(h);
-      break;
-    default:
-      throw invalid_argument("Unknown activation function");
-      break;
-    }
-  }
+	inline Expression activate(Expression h, Activation f) {
+		switch (f) {
+		case LINEAR:
+			return h;
+			break;
+		case RELU:
+			return rectify(h);
+			break;
+		case SIGMOID:
+			return logistic(h);
+			break;
+		case TANH:
+			return tanh(h);
+			break;
+		case SOFTMAX:
+			return softmax(h);
+			break;
+		default:
+			throw invalid_argument("Unknown activation function");
+			break;
+		}
+	}
 };
 
 #endif
--- siamese_network.cc
+++ siamese_network.cc
@@ -1,4 +1,9 @@
-#include "utils.h"
+/**
+ * Train a multilayer perceptron to classify mnist digits
+ *
+ * This provide an example of usage of the mlp.h model
+ */
+#include "mlp.h"
 #include "dynet/io.h"
 #include "dynet/../examples/cpp-utils/getpid.h"
 #include "dynet/../examples/cpp-utils/cl-args.h"
@@ -16,31 +21,45 @@ int main(int argc, char** argv) {
 
     get_args(argc, argv, params, TRAIN_SUP);
 
-    vector<vector<string>> train_labels = vector<vector<string>> ();
-    vector<vector<float>> train_data = vector<vector<float>> ();
+    // Load Dataset ----------------------------------------------------------------------------------
+    // Load data
+    vector<vector<float>> mnist_train, mnist_dev;
 
-    readTrainData(train_labels, train_data);
+    read_mnist(params.train_file, mnist_train);
+    read_mnist(params.dev_file, mnist_dev);
+
+    // Load labels
+    vector<unsigned> mnist_train_labels, mnist_dev_labels;
+
+    read_mnist_labels(params.train_labels_file, mnist_train_labels);
+    read_mnist_labels(params.dev_labels_file, mnist_dev_labels);
+
+    // ParameterCollection name (for saving) -----------------------------------------------------------------------
+    ostringstream os;
+    // Store a bunch of information in the model name
+    os << params.exp_name
+        << "_" << "mlp"
+        << "_" << 784 << "-" << 512 << "-relu-" << 0.2
+        << "_" << 512 << "-" << 512 << "-relu-" << 0.2
+        << "_" << 512 << "-" << 10 << "-softmax"
+        << "_" << getpid()
+        << ".params";
+    const string fname = os.str();
+    cerr << "Parameters will be written to: " << fname << endl;
+    // Build model -----------------------------------------------------------------------------------
 
     ParameterCollection model;
-
+    // Use Adam optimizer
     AdamTrainer trainer(model);
-    trainer.clip_threshold *= params.BATCH_SIZE;
+  trainer.clip_threshold *= params.BATCH_SIZE;
 
-    MLP nn1(model, vector<Layer>({
-        Layer(/* input_dim */ 10240, /* output_dim */ 5000, /* activation */ RELU, /* dropout_rate */ 0.2),
-        Layer(/* input_dim */ 5000, /* output_dim */ 400, /* activation */ RELU, /* dropout_rate */ 0.2),
-        Layer(/* input_dim */ 400, /* output_dim */ 400, /* activation */ LINEAR, /* dropout_rate */ 0.0)
+    // Create model
+    MLP nn(model, vector<Layer>({
+        Layer(/* input_dim */ 784, /* output_dim */ 512, /* activation */ RELU, /* dropout_rate */ 0.2),
+        Layer(/* input_dim */ 512, /* output_dim */ 512, /* activation */ RELU, /* dropout_rate */ 0.2),
+        Layer(/* input_dim */ 512, /* output_dim */ 10, /* activation */ LINEAR, /* dropout_rate */ 0.0)
     }));
 
-    MLP nn2(model, vector<Layer>({
-        Layer(/* input_dim */ 10240, /* output_dim */ 5000, /* activation */ RELU, /* dropout_rate */ 0.2),
-        Layer(/* input_dim */ 5000, /* output_dim */ 400, /* activation */ RELU, /* dropout_rate */ 0.2),
-        Layer(/* input_dim */ 400, /* output_dim */ 400, /* activation */ LINEAR, /* dropout_rate */ 0.0)
-    }));
-
-    MLP nn_join(model, vector<Layer>({
-        Layer(/* input_dim */ 400, /* output_dim */ 1, /* activation */ SIGMOID, /* dropout_rate */ 0.2),
-    }));
 
     // Load preexisting weights (if provided)
     if (params.model_file != "") {
@@ -53,7 +72,7 @@ int main(int argc, char** argv) {
     double worst = 0;
 
     // Number of batches in training set
-    unsigned num_batches = train_data.size()  / params.BATCH_SIZE - 1;
+    unsigned num_batches = mnist_train.size()  / params.BATCH_SIZE - 1;
 
     // Random indexing
     unsigned si;
@@ -62,9 +81,9 @@ int main(int argc, char** argv) {
 
     unsigned epoch = 0;
     vector<Expression> cur_batch;
-    vector<string> cur_labels;
+    vector<unsigned> cur_labels;
 
-    //// static_cast<int> -> change value epoch to int
+    // Run for the given number of epochs (or indefinitely if params.NUM_EPOCHS is negative)
     while (static_cast<int>(epoch) < params.NUM_EPOCHS || params.NUM_EPOCHS < 0) {
         // Reshuffle the dataset
         cerr << "**SHUFFLE\n";
@@ -77,24 +96,78 @@ int main(int argc, char** argv) {
         std::unique_ptr<Timer> iteration(new Timer("completed in"));
 
         // Activate dropout
-        nn1.enable_dropout();
-        nn2.enable_dropout();
-        nn_join.enable_dropout();
+        nn.enable_dropout();
 
         for (si = 0; si < num_batches; ++si) {
             // build graph for this instance
             ComputationGraph cg;
             // Compute batch start id and size
             int id = order[si] * params.BATCH_SIZE;
-            unsigned bsize = std::min((unsigned) train_data.size() - id, params.BATCH_SIZE);
+            unsigned bsize = std::min((unsigned) mnist_train.size() - id, params.BATCH_SIZE);
             // Get input batch
             cur_batch = vector<Expression>(bsize);
             cur_labels = vector<unsigned>(bsize);
             for (unsigned idx = 0; idx < bsize; ++idx) {
-                cur_batch[idx] = input(cg, {10240}, train_data[id + idx]);
-                cur_labels[idx] = train_labels[id + idx][1];
+                cur_batch[idx] = input(cg, {784}, mnist_train[id + idx]);
+                cur_labels[idx] = mnist_train_labels[id + idx];
             }
-            ////
+            // Reshape as batch (not very intuitive yet)
+            Expression x_batch = reshape(concatenate_cols(cur_batch), Dim({784}, bsize));
+            // Get negative log likelihood on batch
+            Expression loss_expr = nn.get_nll(x_batch, cur_labels, cg);
+            // Get scalar error for monitoring
+            loss += as_scalar(cg.forward(loss_expr));
+            // Increment number of samples processed
+            num_samples += bsize;
+            // Compute gradient with backward pass
+            cg.backward(loss_expr);
+            // Update parameters
+            trainer.update();
+            // Print progress every tenth of the dataset
+            if ((si + 1) % (num_batches / 10) == 0 || si == num_batches - 1) {
+                // Print informations
+                trainer.status();
+                cerr << " E = " << (loss / num_samples) << ' ';
+                // Reinitialize timer
+                iteration.reset(new Timer("completed in"));
+                // Reinitialize loss
+                loss = 0;
+                num_samples = 0;
+            }
         }
+
+        // Disable dropout for dev testing
+        nn.disable_dropout();
+
+        // Show score on dev data
+        if (si == num_batches) {
+            double dpos = 0;
+            for (unsigned i = 0; i < mnist_dev.size(); ++i) {
+                // build graph for this instance
+                ComputationGraph cg;
+                // Get input expression
+                Expression x = input(cg, {784}, mnist_dev[i]);
+                // Get negative log likelihood on batch
+                unsigned predicted_idx = nn.predict(x, cg);
+                // Increment count of positive classification
+                if (predicted_idx == mnist_dev_labels[i])
+                dpos++;
+            }
+            // If the dev loss is lower than the previous ones, save the model
+            if (dpos > worst) {
+                worst = dpos;
+                TextFileSaver saver(fname);
+                saver.save(model);
+            }
+            // Print informations
+            cerr << "\n***DEV [epoch=" << (epoch)
+                << "] E = " << (dpos / (double) mnist_dev.size()) << ' ';
+            // Reinitialize timer
+            iteration.reset(new Timer("completed in"));
+        }
+
+        // Increment epoch
+        ++epoch;
+
     }
 }
