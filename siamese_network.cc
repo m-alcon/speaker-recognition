f03ef5f8238513063f04f30868e4d9e59d72f450
miquelalcon <miquel.1996@gmail.com>
2018-06-01 17:27:54 +0200

    validation at the end of epoch

--- siamese_network.cc
+++ siamese_network.cc
@@ -71,97 +71,98 @@ int main(int argc, char** argv) {
         ifstream train_file ("./scripts/train.dat", ifstream::in);
         string speaker1, speaker2;
         float label;
+
+        double loss = 0;
+        unsigned i = 0;
+
+        // Start timer
+        std::unique_ptr<Timer> iteration(new Timer("completed in"));
+
         // Run for the given number of epochs (or indefinitely if params.NUM_EPOCHS is negative)
         //while (static_cast<int>(epoch) < params.NUM_EPOCHS || params.NUM_EPOCHS < 0) {
         while(!train_file.eof()) {
-            double loss = 0;
-
-            // Start timer
-            std::unique_ptr<Timer> iteration(new Timer("completed in"));
 
             // Activate dropout
             nn.enable_dropout();
             cerr << "[STARTING TRAIN PHASE]" << endl;
-            for (int i = 0; i < verification_stop; ++i) {
-                // build graph for this instance
-                ComputationGraph cg;
-                // Get input batch
-                cur_batch1 = vector<Expression>(batch_size);
-                cur_batch2 = vector<Expression>(batch_size);
-                cur_labels = vector<float>(batch_size);
-                for (int j = 0; j < batch_size; ++j) {
-                    train_file >> speaker1 >> speaker2 >> label;
-                    cur_batch1[j] = input(cg, {16896}, readSpeakerFile(speaker1));
-                    cur_batch2[j] = input(cg, {16896}, readSpeakerFile(speaker2));
-                    cur_labels[j] = label;
-                }
-                // Reshape as batch (not very intuitive yet)
-                Expression x1_batch = reshape(concatenate_cols(cur_batch1), Dim({16896}, batch_size));
-                Expression x2_batch = reshape(concatenate_cols(cur_batch2), Dim({16896}, batch_size));
-                // Get negative log likelihood on batch
-                Expression labels_batch = reshape(input(cg, {batch_size}, cur_labels), Dim({1}, batch_size));
-                Expression loss_expr = nn.get_nll(x1_batch, x2_batch, labels_batch, cg);
-                // Get scalar error for monitoring
-                loss = as_scalar(cg.forward(loss_expr));
-                // Increment number of samples processed
-                // Compute gradient with backward pass
-                cg.backward(loss_expr);
-                // Update parameters
-                trainer.update();
-                // Print progress every tenth of the dataset
-                //if (i % 3 == 0 || i == verification_stop) {
-                    // Print informations
-                    //trainer.status();
-                    cerr << "\r[Process: " << i*100/verification_stop << "%]" << " Loss = " << (loss / batch_size);
-                    // Reinitialize timer
-                    //iteration.reset(new Timer("completed in"));
-                    // Reinitialize loss
-                //}
+            // build graph for this instance
+            ComputationGraph cg;
+            // Get input batch
+            cur_batch1 = vector<Expression>(batch_size);
+            cur_batch2 = vector<Expression>(batch_size);
+            cur_labels = vector<float>(batch_size);
+            for (int j = 0; j < batch_size; ++j) {
+                train_file >> speaker1 >> speaker2 >> label;
+                cur_batch1[j] = input(cg, {16896}, readSpeakerFile(speaker1));
+                cur_batch2[j] = input(cg, {16896}, readSpeakerFile(speaker2));
+                cur_labels[j] = label;
             }
-            cerr << endl;
-            train_file.close();
-
-            ifstream test_file ("./scripts/test.dat", ifstream::in);
-            // Disable dropout for dev testing
-            nn.disable_dropout();
-
-            // Show score on dev data
-            double dpos = 0;
-            unsigned train_size = 0;
-            unsigned sum_prediction = 0;
-            cerr << "[STARTING TEST PHASE]" << endl;
-            while (test_file >> speaker1 >> speaker2 >> label) {
-                // build graph for this instance
-                ComputationGraph cg;
-                // Get input expression
-                Expression x1 = input(cg, {16896}, readSpeakerFile(speaker1));
-                Expression x2 = input(cg, {16896}, readSpeakerFile(speaker2));
-                // Get negative log likelihood on batch
-                unsigned predicted_idx = nn.predict(x1, x2, cg);
-                // Increment count of positive classification
-                sum_prediction += predicted_idx;
-                if (predicted_idx == label) {
-                    dpos++;
-                }
-                //if (train_size % 69 == 0) {
-                    cerr << "\r[Process: " << train_size*100/1380 << "%]";
-                //}
-                ++train_size;
-            }
-            cerr << endl;
-            // If the dev loss is lower than the previous ones, save the model
-            if (dpos > worst) {
-                worst = dpos;
-                //TextFileSaver saver(fname);
-                //saver.save(model);
-            }
-            // Print informations
-            cerr << "\n***DEV [epoch=" << (epoch)
-                << "] Accuracy = " << (dpos / (double) train_size) << " | " << sum_prediction << ' ';
-            // Reinitialize timer
-            iteration.reset(new Timer("completed in"));
-            test_file.close();
-
+            // Reshape as batch (not very intuitive yet)
+            Expression x1_batch = reshape(concatenate_cols(cur_batch1), Dim({16896}, batch_size));
+            Expression x2_batch = reshape(concatenate_cols(cur_batch2), Dim({16896}, batch_size));
+            // Get negative log likelihood on batch
+            Expression labels_batch = reshape(input(cg, {batch_size}, cur_labels), Dim({1}, batch_size));
+            Expression loss_expr = nn.get_nll(x1_batch, x2_batch, labels_batch, cg);
+            // Get scalar error for monitoring
+            loss = as_scalar(cg.forward(loss_expr));
+            // Increment number of samples processed
+            // Compute gradient with backward pass
+            cg.backward(loss_expr);
+            // Update parameters
+            trainer.update();
+            // Print progress every tenth of the dataset
+            //if (i % 3 == 0 || i == verification_stop) {
+                // Print informations
+                //trainer.status();
+                cerr << "\r[Process: " << i*100/(2760/20) << "%]" << " Loss = " << (loss / batch_size);
+                // Reinitialize timer
+                //iteration.reset(new Timer("completed in"));
+                // Reinitialize loss
+            //}
+            ++i;
         }
+        cerr << endl;
+        train_file.close();
+
+        ifstream test_file ("./scripts/test.dat", ifstream::in);
+        // Disable dropout for dev testing
+        nn.disable_dropout();
+
+        // Show score on dev data
+        double dpos = 0;
+        unsigned train_size = 0;
+        unsigned sum_prediction = 0;
+        cerr << "[STARTING TEST PHASE]" << endl;
+        while (test_file >> speaker1 >> speaker2 >> label) {
+            // build graph for this instance
+            ComputationGraph cg;
+            // Get input expression
+            Expression x1 = input(cg, {16896}, readSpeakerFile(speaker1));
+            Expression x2 = input(cg, {16896}, readSpeakerFile(speaker2));
+            // Get negative log likelihood on batch
+            unsigned predicted_idx = nn.predict(x1, x2, cg);
+            // Increment count of positive classification
+            sum_prediction += predicted_idx;
+            if (predicted_idx == label) {
+                dpos++;
+            }
+            //if (train_size % 69 == 0) {
+                cerr << "\r[Process: " << train_size*100/1380 << "%]";
+            //}
+            ++train_size;
+        }
+        cerr << endl;
+        // If the dev loss is lower than the previous ones, save the model
+        if (dpos > worst) {
+            worst = dpos;
+            TextFileSaver saver(fname);
+            //saver.save(model);
+        }
+        // Print informations
+        cerr << "\n***DEV [epoch=" << (epoch)
+            << "] Accuracy = " << (dpos / (double) train_size) << " | " << sum_prediction << ' ';
+        // Reinitialize timer
+        iteration.reset(new Timer("completed in"));
+        test_file.close();
     }
 }
