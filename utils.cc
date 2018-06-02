3e51a330e8b70cb94cdac6a21f2b1c2fc9fb0c69
miquelalcon <miquel.1996@gmail.com>
2018-06-02 19:03:57 +0200

    checking utils

--- utils.cc
+++ utils.cc
@@ -3,10 +3,11 @@
 using namespace std;
 
 int main() {
-    vector<vector<vector<float>>> train_data = loadData("test");
-    Example ex = generateExample(train_data);
-    for(unsigned i = 0; i < (*ex.positive1).size(); i++) {
-        cout << (*ex.positive1)[i] << " ";
+    vector<vector<vector<float>>> test_data = loadData("test");
+    Example ex = generateExample(test_data);
+    for(unsigned i = 0; i < 2000; i++) {
+        cerr << "\r" << i << "/2000" << endl;
+        Example ex = generateExample(test_data);
     }
     cout << endl;
 }
