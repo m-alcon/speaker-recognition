d87060ca8eb80cb1f86943e4f16788c06fecca2d
miquelalcon <miquel.1996@gmail.com>
2018-06-02 13:16:22 +0200

    evaluating utils test data

--- utils.cc
+++ utils.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    vector<vector<vector<float>>> train_data = loadData("train");
+    vector<vector<vector<float>>> train_data = loadData("test");
     vector<float> positive1, positive2, negative1, negative2;
     generateExample(train_data, positive1, positive2, negative1, negative2);
     for(unsigned i = 0; i < positive1.size(); i++) {
